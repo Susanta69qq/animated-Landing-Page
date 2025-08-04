0d617289f85e9b6ce668c57cd80ac06805ef6034
Susanta69qq <susantaray786@gmail.com>
2025-08-04 12:34:46 +0530

    Hero Section animation which includes Text and Image animation now complete ✔

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,21 +13,34 @@ const Hero = () => {
     heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
-        yPercent: 100,
-        duration: 1.8,
-        ease: 'expo.out',
-        stagger: 0.05
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
     });
 
     gsap.from(paragraphSplit.lines, {
-        opacity: 0,
-        yPercent: 100,
-        duration: 1.8,
-        ease: 'expo.out',
-        stagger: 0.05,
-        delay: 1
-    })
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
+      delay: 1,
+    });
 
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf",{ y: 200 }, 0 )
+      .to(".left-leaf", { y: -200 }, 0)
+      
+      
   }, []);
 
   return (
